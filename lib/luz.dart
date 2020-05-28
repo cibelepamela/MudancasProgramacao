3196d81b865a3a8e3672841cd0239860d364e9ef
gsnagal <64446302+gsnagal@users.noreply.github.com>
2020-05-28 16:04:40 -0300

    Add files via upload

--- lib/luz.dart
+++ lib/luz.dart
@@ -1,17 +1,25 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'stopwatch.dart';
 import 'dart:async';
-import 'package:fenrir_software/stopwatch.dart';
 
-
-class luz extends chronometer {
-
+class luz extends StatefulWidget {
   @override
+  _luzState createState() => _luzState();
+}
 
-
+class _luzState extends State<luz> with timeFlag{
+  int getTempo() {
+    // TODO: implement getTempo
+    return super.getTempo();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.white,
-    );
+    setState(() {
+        if (getTempo() == 10000)
+          return Container(color: Colors.white);
+        if (getTempo() == 5000)
+          return Container(color: Colors.black);
+    });
   }
 }
