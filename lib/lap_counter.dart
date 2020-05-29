388733f4862eb7965f408a5ea4b239e6c9d782fd
gsnagal <64446302+gsnagal@users.noreply.github.com>
2020-05-29 16:17:00 -0300

    Add files via upload

--- lib/lap_counter.dart
+++ lib/lap_counter.dart
@@ -18,28 +18,29 @@ class LapCounterState extends State<LapCounter> {
   var geolocator = Geolocator();
   var uuid = Uuid();
   Position lapLocation;
-  Position startPosition;
-  int lap = 0;
-  double distance;
-  var time;
-  var now;
+  Position startPosition1, startPosition2, startPosition3, startPosition4;
+  int lap = 0, set = 0;
+  double distance1, distance2, distance3, distance4;
+  var time1, time2, time3, time4;
+  var now1, now2, now3, now4;
   var round_uuid;
 
   @override
 
   void initState() {
-    // TODO: implement initState
-    super.initState();
-    geolocator
-        .getPositionStream(
-        LocationOptions(accuracy: LocationAccuracy.best, timeInterval: 500))
-        .listen((position) {
-      setState(() {
-        lapLocation = position;
-        if (startPosition != null){
-          volta();
-          post();
-        }
+          // TODO: implement initState
+          super.initState();
+          geolocator
+              .getPositionStream(
+              LocationOptions(accuracy: LocationAccuracy.best, timeInterval: 500))
+              .listen((position) {
+            setState(() {
+              lapLocation = position;
+              if (startPosition1 != null){
+                volta();
+                post();
+                setor();
+              }
       });
     });
   }
@@ -50,19 +51,45 @@ class LapCounterState extends State<LapCounter> {
       "lat": lapLocation.latitude.toString(),
       "lon": lapLocation.longitude.toString(),
       "round_uuid": round_uuid,
-      "vel": (3.6*lapLocation.speed).toStringAsFixed(2)
+      "vel": (3.6*lapLocation.speed).toStringAsFixed(2),
+      "set": set.toString()
     });
     var response = await http.post('http://fenrir.servebeer.com:1996/FenrirApi', body: json, headers: {'Content-Type': 'application/json'});
     print('Response status: ${response.statusCode}');
   }
 
   void volta() async{
-    distance = await geolocator.distanceBetween(startPosition.latitude, startPosition.longitude, lapLocation.latitude, lapLocation.longitude);
-    time = DateTime.now().difference(now).inSeconds;
-    if (distance < 10 && time > 2){
+    distance1 = await geolocator.distanceBetween(startPosition1.latitude, startPosition1.longitude, lapLocation.latitude, lapLocation.longitude);
+    time1 = DateTime.now().difference(now1).inSeconds;
+    if (distance1 < 10 && time1 > 2){
       lap++;
-      now = DateTime.now();
+      set++;
+      now1 = DateTime.now();
 //      print(distance);
+    }
+  }
+
+  void setor() async{
+    distance2 = await geolocator.distanceBetween(startPosition2.latitude, startPosition2.longitude, lapLocation.latitude, lapLocation.longitude);
+    distance3 = await geolocator.distanceBetween(startPosition3.latitude, startPosition3.longitude, lapLocation.latitude, lapLocation.longitude);
+    distance4 = await geolocator.distanceBetween(startPosition4.latitude, startPosition4.longitude, lapLocation.latitude, lapLocation.longitude);
+    time2 = DateTime.now().difference(now2).inSeconds;
+    time3 = DateTime.now().difference(now3).inSeconds;
+    time4 = DateTime.now().difference(now4).inSeconds;
+    if (distance2 < 10 && time2 > 2){
+      set++;
+      now2 = DateTime.now();
+    }
+    if (distance3 < 10 && time3 > 2){
+      set++;
+      now3 = DateTime.now();
+    }
+    if (distance3 < 10 && time3 > 2){
+      set++;
+      now3 = DateTime.now();
+    }
+    if (set > 4){
+      set = 1;
     }
   }
 
@@ -99,9 +126,12 @@ class LapCounterState extends State<LapCounter> {
                   onPressed: ()async{
                     var asinc = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
                     setState(() {
-                      startPosition = asinc;
+                      startPosition1 = asinc;
                       round_uuid = uuid.v4();
-                      now = DateTime.now();
+                      now1 = DateTime.now();
+                      now2 = DateTime.now();
+                      now3 = DateTime.now();
+                      now4 = DateTime.now();
                     });
                   },
                 ),
@@ -116,7 +146,7 @@ class LapCounterState extends State<LapCounter> {
                   ),
                   onPressed: (){
                     setState(() {
-                      startPosition = null;
+                      startPosition1 = null;
                       lap = 0;
                     });
                   },
