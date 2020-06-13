4616ecbeeab142fc516eb43666b08c8c37db5bff
yujisoyama <64661100+yujisoyama@users.noreply.github.com>
2020-06-13 16:53:00 -0300

    Lap counter adicionado ao stopwatch

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,7 +3,6 @@ import 'lap_counter.dart';
 import 'stopwatch.dart';
 import 'location_manager.dart';
 
-
 class HomePage extends StatelessWidget {
   var userLocation;
   @override
@@ -13,28 +12,49 @@ class HomePage extends StatelessWidget {
       //  title: Text("Equipe Fenrir"),
       //),
       body: Center(
-          child: Column(
-              children: <Widget>[
-                Expanded(
-                    flex: 2, // 20%
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 6,
-                          child: Container(
-                            color: Colors.black,
-                            child: locationManager(),
-                          ),
-                        ),
-                      ],
-                    )
-                ),
-                Expanded(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2, // 20%
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                      color: Colors.black,
+                      child: locationManager(),
+                    ),
+                  ),
+                  Expanded(
                     flex: 2,
-                    child: chronometer()
-                ),
-              ])
+                    child: Container(
+                      color: Colors.yellow[100],
+                      child: LapCounter(),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/fenrir.png"),
+                          fit: BoxFit.fill,
+                        ),
+                        color: Colors.yellow[300],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
+            Expanded(
+              flex: 2,
+              child: chronometer()
+              ),
+          ])
       ),
     );
   }
 }
+
+
