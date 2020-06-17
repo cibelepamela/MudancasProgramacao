a3c6af7887698041bfa3869ebc3a9a544fd792ca
gsnagal <64446302+gsnagal@users.noreply.github.com>
2020-06-17 15:01:44 -0300

    Add files via upload

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,49 +12,11 @@ class HomePage extends StatelessWidget {
       //  title: Text("Equipe Fenrir"),
       //),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 2, // 20%
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 6,
-                    child: Container(
-                      color: Colors.black,
-                      child: locationManager(),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      color: Colors.yellow[100],
-                      child: LapCounter(),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/fenrir.png"),
-                          fit: BoxFit.fill,
-                        ),
-                        color: Colors.yellow[300],
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ),
-            Expanded(
-              flex: 2,
-              child: chronometer()
-              ),
-          ])
+          child: Expanded(
+            flex: 1,
+            child: chronometer(),
+          ),
       ),
     );
   }
 }
-
-
--- lib/teste.dart
+++ lib/teste.dart
@@ -14,62 +14,6 @@ class chronometer extends StatefulWidget {
 }
 
 class _chronometerState extends State<chronometer> {
-  bool startispressed = true;
-  bool stopispressed = true;
-  String stoptimetodisplay = '00:00:00';
-  var swatch = Stopwatch();
-  final dur = const Duration(milliseconds: 1);
-  int tempo = 0;
-
-
-  void starttimer(){
-    Timer(dur, keepruning);
-  }
-
-  void keepruning() {
-    if (swatch.isRunning) {
-      starttimer();
-    }
-    setState(() {
-      if (tempo <= 10000) {
-        stoptimetodisplay =
-            swatch.elapsed.inMinutes.toString().padLeft(2, '0') + ':' +
-                (swatch.elapsed.inSeconds % 60).toString().padLeft(2, '0') +
-                ':' +
-                (swatch.elapsed.inMilliseconds % 60).toString().padLeft(2, '0');
-        tempo = swatch.elapsed.inMilliseconds;
-      }
-      else {
-        swatch.reset();
-        tempo = 0;
-      }
-    });
-  }
-
-  void startstopwatch(){
-    setState(() {
-      stopispressed = false;
-      startispressed = false;
-    });
-    swatch.start();
-    starttimer();
-  }
-
-  void stopstopwatch(){
-    setState(() {
-      stopispressed = true;
-      startispressed = true;
-      stoptimetodisplay = '00:00:00';
-    });
-    swatch.stop();
-    swatch.reset();
-  }
-
-
-
-
-
-
   var geolocator = Geolocator();
   var uuid = Uuid();
   Position lapLocation;
@@ -107,7 +51,8 @@ class _chronometerState extends State<chronometer> {
       "lon": lapLocation.longitude.toString(),
       "round_uuid": round_uuid,
       "vel": (3.6*lapLocation.speed).toStringAsFixed(2),
-      "set": set.toString()
+      "set": set.toString(),
+      "time set": time2.toString(),
     });
     var response = await http.post('http://fenrir.servebeer.com:1996/FenrirApi', body: json, headers: {'Content-Type': 'application/json'});
     print('Response status: ${response.statusCode}');
@@ -115,8 +60,8 @@ class _chronometerState extends State<chronometer> {
 
   void volta() async{
     distance1 = await geolocator.distanceBetween(startPosition1.latitude, startPosition1.longitude, lapLocation.latitude, lapLocation.longitude);
-    time1 = DateTime.now().difference(now1).inSeconds;
-    if (distance1 < 10 && time1 > 2){
+    time1 = DateTime.now().difference(now1).inMilliseconds;
+    if (distance1 < 10 && time1 > 2000){
       lap++;
       set++;
       now1 = DateTime.now();
@@ -129,16 +74,16 @@ class _chronometerState extends State<chronometer> {
     distance2 = await geolocator.distanceBetween(startPosition2.latitude, startPosition2.longitude, lapLocation.latitude, lapLocation.longitude);
     distance3 = await geolocator.distanceBetween(startPosition3.latitude, startPosition3.longitude, lapLocation.latitude, lapLocation.longitude);
     distance4 = await geolocator.distanceBetween(startPosition4.latitude, startPosition4.longitude, lapLocation.latitude, lapLocation.longitude);
-    time2 = DateTime.now().difference(now2).inSeconds;
-    if (distance2 < 10 && time2 > 2){
+    time2 = DateTime.now().difference(now2).inMilliseconds;
+    if (distance2 < 10 && time2 > 2000){
       set++;
       now2 = DateTime.now();
     }
-    if (distance3 < 10 && time2 > 2){
+    if (distance3 < 10 && time2 > 2000){
       set++;
       now2 = DateTime.now();
     }
-    if (distance3 < 10 && time2 > 2){
+    if (distance3 < 10 && time2 > 2000){
       set++;
       now2 = DateTime.now();
     }
@@ -151,121 +96,212 @@ class _chronometerState extends State<chronometer> {
 
 
 
+
+  bool startispressed = true;
+  bool stopispressed = true;
+  String stoptimetodisplay = '00:00:00';
+  var swatch = Stopwatch();
+  var swa = new Stopwatch();
+  final dur = const Duration(milliseconds: 1);
+  int tempo = 0;
+  int tempo1 = 0;
+  int tempo2 = 0;
+  double velocidade;
+  int timeSetor;
+  int i = 1;
+
+
+  void starttimer(){
+    Timer(dur, keepruning);
+  }
+
+  void keepruning() {
+    if (swatch.isRunning) {
+      starttimer();
+    }
+    setState(() {
+      if (tempo <= 10000) {
+        stoptimetodisplay =
+            swatch.elapsed.inMinutes.toString().padLeft(2, '0') + ':' +
+                (swatch.elapsed.inSeconds % 60).toString().padLeft(2, '0') +
+                ':' +
+                (swatch.elapsed.inMilliseconds % 60).toString().padLeft(2, '0');
+        tempo = swatch.elapsed.inMilliseconds;
+      }
+      else {
+        swatch.reset();
+        tempo = 0;
+      }
+    });
+  }
+
+  void startstopwatch() async{
+    var asinc = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    swatch.start();
+    swa.start();
+    starttimer();
+    setState(() {
+      startPosition1 = asinc;
+      round_uuid = uuid.v4();
+      now1 = DateTime.now();
+      now2 = DateTime.now();
+      stopispressed = false;
+      startispressed = false;
+    });
+  }
+
+  void stopstopwatch(){
+    setState(() {
+      startPosition1 = null;
+      lap = 0;
+      stopispressed = true;
+      startispressed = true;
+      stoptimetodisplay = '00:00:00';
+    });
+    swatch.stop();
+    swatch.reset();
+    swa.stop();
+    swa.reset();
+  }
+
+  void startstopwatch_vel() async{
+    velocidade = lapLocation.speed*3.6;
+    if(velocidade > 5){
+      var asinc = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+      swatch.start();
+      starttimer();
+      setState(() {
+        startPosition1 = asinc;
+        round_uuid = uuid.v4();
+        now1 = DateTime.now();
+        now2 = DateTime.now();
+        stopispressed = false;
+        startispressed = false;
+      });
+    }
+  }
+
+
+
   @override
 
   Widget build(BuildContext context) {
     return Container(
-      color:
+        color:
         tempo >=5000
-        ? Colors.white
-        :Colors.black,
-    child:
-      Column(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                flex: 6,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    stoptimetodisplay,
-                    style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w700,
-                        color: tempo >=5000
-                          ? Colors.black
-                          :Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                  alignment: Alignment.center,
-                  child:
+            ? Colors.white
+            : Colors.yellow[300],
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image:  AssetImage("assets/images/fenrir.png"),
+                fit: BoxFit.cover
+            )
+        ),
+        child:
+        Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
                   lapLocation == null
                       ? CircularProgressIndicator()
-                      : Text(
-                      lap.toString() + " laps",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w700,
-                        color: tempo >=5000
-                          ? Colors.black
-                          :Colors.white,
-                      )),
-                ),
-              )
-            ],
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  child:
-                FlatButton(
-                    child: Icon(
-                      Icons.play_arrow,
-                      color: Colors.green,
-                    ),
-                  onPressed: () async{
-                    var asinc = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-                    setState(() {
-                      startPosition1 = asinc;
-                      round_uuid = uuid.v4();
-                      now1 = DateTime.now();
-                      now2 = DateTime.now();
-                      startispressed
-                          ? startstopwatch
-                          : null;
-                    });
-                  },
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 50.0,
-                    vertical: 2.0,
-                  ),
-                ),
-              )
+                      : Text((3.6*lapLocation.speed).toStringAsFixed(1) + " km/h",
+                      style: TextStyle(fontSize: 100,
+                        color:
+                        Colors.black,)),
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  child:
-                FlatButton(
-                    child: Icon(
-                      Icons.stop,
-                      color: Colors.red,
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        stoptimetodisplay + '\n' + tempo1.toString(),
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
-                  onPressed: (){
-                    setState(() {
-                      startPosition1 = null;
-                      lap = 0;
-                      stopispressed
-                          ? null
-                          : stopstopwatch;
-                    });
-                  },
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 50.0,
-                    vertical: 2.0,
                   ),
-                )
-                ),
-              )
-            ],
-          ),
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child:
+                      lapLocation == null
+                          ? CircularProgressIndicator()
+                          : Text(
+                          lap.toString() + " laps",
+                          style: TextStyle(
+                            fontSize: 50.0,
+                            fontWeight: FontWeight.w700,
+                            color:
+                                Colors.black,
+                          )),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        child:
+                        FlatButton(
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.green,
+                          ),
+                          onPressed:
+                          startispressed
+                              ? startstopwatch
+                              : null,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 50.0,
+                            vertical: 2.0,
+                          ),
+                        ),
+                      )
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                        child:
+                        FlatButton(
+                          child: Icon(
+                            Icons.stop,
+                            color: Colors.red,
+                          ),
+                          onPressed:
+                          stopispressed
+                              ? null
+                              : stopstopwatch,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 50.0,
+                            vertical: 2.0,
+                          ),
+                        )
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         )
-      ],
-    )
     );
   }
 }
